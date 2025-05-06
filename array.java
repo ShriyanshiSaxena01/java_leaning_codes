399371d6d7099f2faf68a5370de15d9b88be0b9a
Shriyanshi saxena <shriyanshisaxena01@gmail.com>
2025-05-07 03:36:52 +0530

    COMMIT

--- array.java
+++ array.java
@@ -18,16 +18,122 @@
 
 
 
+// class array {
+    
+//     public static void main(String[] args) {
+        
+//         int arr[]=new int[5];
+
+//        arr[1]= 10;
+//        arr[4]= -10;
+
+//         System.out.println(arr[0]);
+//         System.out.println(arr[1]);
+//         System.out.println(arr[2]);
+//         System.out.println(arr[3]);
+//         System.out.println(arr[4]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// class array {
+    
+//     public static void main(String[] args) {
+        
+        
+
+//        int arr[]={10,20,30,40,50};
+      
+
+//         System.out.println(arr[0]);
+//         System.out.println(arr[1]);
+//         System.out.println(arr[2]);
+//         System.out.println(arr[3]);
+//         System.out.println(arr[4]);
+//     }
+// }
+
+
+
+
+
+// import java.util.Scanner;
+
+// class array {
+    
+//     public static void main(String[] args) {
+        
+//         int arr[]=new int[5];
+//         Scanner sk=new Scanner(System.in);
+
+
+
+//         System.out.println("Enter a index of 0 : ");
+//         arr[0]=sk.nextInt();
+
+//         System.out.println("Enter a index of 1 : ");
+//         arr[1]=sk.nextInt();
+
+//         System.out.println("Enter a index of 2 : ");
+//         arr[2]=sk.nextInt();
+
+//         System.out.println("Enter a index of 3 : ");
+//         arr[3]=sk.nextInt();
+
+//         System.out.println("Enter a index of 4 : ");
+//         arr[4]=sk.nextInt();
+
+
+//         System.out.println(arr[0]);
+//         System.out.println(arr[1]);
+//         System.out.println(arr[2]);
+//         System.out.println(arr[3]);
+//         System.out.println(arr[4]);
+//         System.out.println("length of array :" + arr.length);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
 class array {
     
     public static void main(String[] args) {
         
-        int arr[]=new int[5];
+        float arr=new nextf
+        Scanner sk=new Scanner(System.in);
 
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(arr[3]);
-        System.out.println(arr[4]);
+char ny ='b';
+       int i;
+
+       for (i=0; i<arr.length;i++)
+       {
+        System.out.println("Enter a index of  "+i+ " : ");
+        arr[i]=sk.nextInt();
+       }
+
+       for (i=0; i<arr.length;i++)
+       {
+        System.out.println("\t"+arr[i]);
+        System.out.println("\t"+arr[i]);
+
+        float 
+
+       }
+       
     }
 }
